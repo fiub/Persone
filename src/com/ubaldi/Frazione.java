a38026921d647a5cffcdb0c4517e24aeb6a7c66e
Filippo <filippo.ubaldi@roialty.com>
2018-12-06 17:36:21 +0100

    frazione 3

--- src/com/ubaldi/Frazione.java
+++ src/com/ubaldi/Frazione.java
@@ -8,7 +8,7 @@ import static java.lang.System.out;
 public class Frazione {
     public int num;
     public int den;
-    public int mcd;
+    private int mcd;
 
     public int getCalcolaMCD(int a, int b) {
         int r;
@@ -21,17 +21,30 @@ public class Frazione {
         return a; //... e quando il resto n-esimo è 0, l'mcd è il resto n-1esimo
     }
 
+    /*
+
     @Contract(pure = true)
     private int reduceFraction(int n, int mcd) {
-        n = n/mcd;
-        return n;
+    n = n/mcd;
+    return n;
+    }
+
+    */
+
+    public String reduceFraction(){
+        this.num = num;
+        this.den = den;
+        this.mcd = mcd;
+        String frac = Integer.toString(num/mcd) + "/" + Integer.toString(den/mcd);
+        final String frac1 = frac;
+        return frac1;
     }
 
     public void stampaSuConsole() {
 
         mcd = getCalcolaMCD(num,den);
         out.println("------------");
-        out.println(num + "/" + den + " si riduce a: " + reduceFraction(num,mcd) + "/" + reduceFraction(den,mcd));
+        out.println(num + "/" + den + " si riduce a: " +  reduceFraction());
         out.println("mcd = " + mcd);
         out.println("------------");
 
