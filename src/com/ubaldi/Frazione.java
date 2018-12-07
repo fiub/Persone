51c7b6da39f73db0663192da0c001588498f8200
Filippo <filippo.ubaldi@roialty.com>
2018-12-07 16:00:49 +0100

    frazione stessa allocazione di memo

--- src/com/ubaldi/Frazione.java
+++ src/com/ubaldi/Frazione.java
@@ -7,9 +7,6 @@ import static java.lang.System.out;
 public class Frazione {
     public int num;
     public int den;
-    private int sopra;
-    private int sotto;
-    private int mcd;
 
     @Contract(pure = true)
     private int getCalcolaMCD(int a, int b) {
@@ -23,40 +20,25 @@ public class Frazione {
         return a; //... e quando il resto n-esimo è 0, l'mcd è il resto n-1esimo
     }
 
-    /*
-
-    @Contract(pure = true)
-    private int reduceFraction(int n, int mcd) {
-    n = n/mcd;
-    return n;
-    }
-
-    */
-
-    private String reduceFraction(){
+    public void reduceFraction(){
         this.num = num;
         this.den = den;
-        this.mcd = mcd;
-        this.sopra = sopra;
-        this.sotto = sotto;
-        switch (sopra = num / mcd) {
-        }
-        switch (sotto = den / mcd) {
-        }
-        String frac = Integer.toString(num/mcd) + "/" + Integer.toString(den/mcd);
-        final String frac1 = frac;
-        return frac1;
+        int mcd = getCalcolaMCD(num, den);
+        num = num / mcd;
+        den = den / mcd;
+        out.println("------------");
+        out.println("La frazione si riduce a: " +  num + "/" + den);
     }
 
-    public void stampaSuConsole() {
+    /*
+public void stampaSuConsole() {
 
-        mcd = getCalcolaMCD(num,den);
-        out.println("------------");
-        out.println(num + "/" + den + " si riduce a: " +  reduceFraction());
-        out.println(num + "/" + den + " si riduce a: " +  sopra + "/" + sotto);
-        out.println("mcd = " + mcd);
-        out.println("------------");
+out.println("------------");
+out.println(num + "/" + den + " si riduce a: " +  num + "/" + den);
+out.println("mcd = " + mcd);
+out.println("------------");
 
-    }
+}
+*/
 
 }
